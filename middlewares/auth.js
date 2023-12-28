441b91dd1640a94b9d5b3f78015d49b999d3b33a
Anzhelika <angelikachashchina@gmail.com>
2023-12-29 01:45:18 +0300

    Tests 14

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,23 +5,30 @@ const { JWT_SECRET = 'SECRET_KEY' } = process.env;
 const auth = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization) {
-    return res.status(401).json({ message: 'Необходима авторизация' });
+    return res.status(401).send({ message: 'Необходима авторизация' });
   }
+
   const token = authorization.replace('Bearer ', '');
   let payload;
   try {
     payload = jwt.verify(token, JWT_SECRET);
   } catch (error) {
-    if (error instanceof jwt.JsonWebTokenError) {
-      return res.status(401).json({ message: 'С токеном что-то не то' });
+    if (error.message === 'NotAutanticate') {
+      return res
+        .status(401)
+        .send({ message: 'Неправильный email или пароль' });
     }
-    return res.status(401).json({ message: 'Необходима авторизация' });
-  }
-  if (!payload.userId) {
-    return res.status(401).json({ message: 'Необходима авторизация' });
+    if (error.message === 'JsonWebTokenError') {
+      return res
+        .status(401)
+        .send({ message: 'С токеном что-то не то' });
+    }
+    return res
+      .status(500)
+      .send({ message: 'Сервер отвалился' });
   }
   req.user = payload;
-  next();
+  return next();
 };
 
 module.exports = auth;
