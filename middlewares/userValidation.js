42a4a8b0fc4f53564becbebbc5e2d4601adb85a3
Anzhelika <angelikachashchina@gmail.com>
2023-12-29 14:15:54 +0300

    Tests 14

---
+++ middlewares/userValidation.js
@@ -0,0 +1,45 @@
+const { celebrate, Joi } = require('celebrate');
+
+const validateUserInfo = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string(),
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+});
+
+const validateUserAuthentication = celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(6),
+  }),
+});
+
+const validateUserUpdate = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().required().min(2).max(30),
+  }),
+});
+
+const validateAvatarUpdate = celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().required(),
+  }),
+});
+
+const validateUserId = celebrate({
+  params: Joi.object().keys({
+    userId: Joi.string().required().hex().length(24),
+  }),
+});
+
+module.exports = {
+  validateAvatarUpdate,
+  validateUserAuthentication,
+  validateUserId,
+  validateUserInfo,
+  validateUserUpdate,
+};
--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,21 @@ const { Router } = require('express');
 const { userRouter } = require('./users');
 const { cardRouter } = require('./cards');
 const { login, createUser } = require('../controllers/users');
+const {
+  validateUserAuthentication,
+  validateUserInfo,
+} = require('../middlewares/userValidation');
 
 const auth = require('../middlewares/auth');
 
 const router = Router();
-router.use('/users', userRouter);
-router.use('/cards', cardRouter);
-
-router.post('/signup', createUser);
-router.post('/signin', login);
+router.post('/signup', validateUserInfo, createUser);
+router.post('/signin', validateUserAuthentication, login);
 
 router.use(auth);
+
+router.use('/users', userRouter);
+router.use('/cards', cardRouter);
 
 router.all('/*', (req, res) => {
   res.status(404).send({ message: 'Ресурc не найден' });
