6d1fb5cfb65c5293f11d9532b3e66efc2e8f19c1
Anzhelika <angelikachashchina@gmail.com>
2023-12-29 15:06:09 +0300

    Tests 14

--- middlewares/userValidation.js
+++ middlewares/userValidation.js
@@ -6,7 +6,7 @@ const validateUserInfo = celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string(),
+    avatar: Joi.string().pattern(URL),
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
   }),
